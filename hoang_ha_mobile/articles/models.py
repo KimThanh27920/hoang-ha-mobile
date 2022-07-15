b2afc2783fb71f58c17f9ee30e1f87f027bbaff1
tantrung3t <tantrung.dmc@gmail.com>
2022-07-15 11:10:18 +0700

    update field for articles models app

--- hoang_ha_mobile/articles/models.py
+++ hoang_ha_mobile/articles/models.py
@@ -11,6 +11,9 @@ class Tag(models.Model):
     updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_update_tag", null=True)
     deleted_at = models.DateTimeField(blank=True, null=True)
     deleted_by = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="user_delete_tag", null=True)
+
+    def __str__(self):
+        return self.name
     
 class Article(models.Model):
     title = models.CharField(max_length=255)
@@ -18,6 +21,7 @@ class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articles")
     viewer = models.IntegerField()
     status = models.BooleanField(default=False)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_create_article", null=True)
@@ -26,3 +30,5 @@ class Article(models.Model):
     deleted_at = models.DateTimeField(blank=True, null=True)
     deleted_by = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="user_delete_article", null=True)
     
+    def __str__(self):
+        return self.title
