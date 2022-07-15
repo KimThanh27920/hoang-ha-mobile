0fe88f67c5922a6cd94aa4db1e95bf3e009df4a0
tantrung3t <tantrung.dmc@gmail.com>
2022-07-15 11:21:21 +0700

    update for articles models 11:21 15/7/2022

--- hoang_ha_mobile/articles/models.py
+++ hoang_ha_mobile/articles/models.py
@@ -19,14 +19,14 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articles")
-    viewer = models.IntegerField()
+    viewer = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_create_article", null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
-    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_update_article", null=True)
+    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="user_update_article", null=True)
     deleted_at = models.DateTimeField(blank=True, null=True)
     deleted_by = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="user_delete_article", null=True)
     
