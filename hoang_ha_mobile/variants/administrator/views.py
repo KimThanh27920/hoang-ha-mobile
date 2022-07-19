815fd6041b5f3fff64401a37cd45d6026a184ba9
Kim Thanh <thanh27920@gmail.com>
2022-07-19 16:30:46 +0700

    update model products and variants

---
+++ hoang_ha_mobile/variants/administrator/views.py
@@ -0,0 +1,67 @@
+from itertools import product
+from rest_framework import viewsets, filters
+from rest_framework.permissions import IsAdminUser
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.response import Response
+
+from variants.serializers import VariantSerializer,VariantReadSerializer
+from variants.models import Variant
+
+from django_filters.rest_framework import DjangoFilterBackend
+from datetime import datetime
+
+class VariantViewSet(viewsets.ModelViewSet):
+    serializer_class = VariantSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminUser]
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    #search_fields = ['product__id']
+    filterset_fields = ['product','status']
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return VariantReadSerializer
+        return VariantSerializer
+    
+    # def get_queryset(self):
+    #     if self.request.method == 'GET':
+    #         if self.kwargs['product_id'] :
+    #             self.queryset = Variant.objects.filter(product=self.kwargs['product_id']).select_related('product')
+    #         return Variant.objects.all().select_related('product')
+    #     return Variant.objects.all()
+    def get_queryset(self):
+        if self.request.method == 'GET':
+           return Variant.objects.all().select_related('product') 
+        return Variant.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+        serializer.save(updated_by=self.request.user)
+    
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', True)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        serializer.save(updated_by=self.request.user)
+
+    def perform_destroy(self, instance):
+        data = {
+            "id": instance.id,
+            "deleted_by": self.request.user.id,
+            "deleted_at": datetime.now()
+        }
+        serializer = VariantSerializer(instance=instance, data=data,partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
---
+++ hoang_ha_mobile/variants/serializers.py
@@ -0,0 +1,67 @@
+import imp
+from itertools import product
+from rest_framework import serializers
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAdminUser
+
+from variants.models import Variant
+from products.serializers import ProductSerializer
+
+class VariantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Variant
+        fields = [
+            'id',
+            'product',
+            'color',
+            'version',
+            'size',
+            'strap',
+            'general',
+            'utilities',
+            'network',
+            'storage',
+            'os_cpu',
+            'front_cam',
+            'camera',
+            'pin',
+            'screen',
+            'price',
+            'sale',
+            'created_at',
+            'created_by',
+            'updated_at',
+            'updated_by',
+            'deleted_at',
+            'deleted_by',
+        ]
+
+class VariantReadSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+    class Meta:
+        model = Variant
+        fields = [
+            'id',
+            'product',
+            'color',
+            'version',
+            'size',
+            'strap',
+            'general',
+            'utilities',
+            'network',
+            'storage',
+            'os_cpu',
+            'front_cam',
+            'camera',
+            'pin',
+            'screen',
+            'price',
+            'sale',
+            'created_at',
+            'created_by',
+            'updated_at',
+            'updated_by',
+            'deleted_at',
+            'deleted_by',
+        ]
