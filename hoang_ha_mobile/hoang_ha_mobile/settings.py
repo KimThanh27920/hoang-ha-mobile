d8ffa03da33e72afc20572d5ac552a8733d0f219
Toan <crisriorock0@gmail.com>
2022-07-19 08:32:06 +0700

    add packet JWT and update REST_FRAMEWORK into setting file

--- hoang_ha_mobile/hoang_ha_mobile/settings.py
+++ hoang_ha_mobile/hoang_ha_mobile/settings.py
@@ -46,8 +46,23 @@ INSTALLED_APPS = [
     'comments',
     'orders',
     'products',
-    'variants'
+    'variants',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],    
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
