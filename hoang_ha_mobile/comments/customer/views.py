0f886418f9c31df592eaefe0581f8298e70a4504
Toan <crisriorock0@gmail.com>
2022-07-19 16:59:26 +0700

    create comments, carts

--- hoang_ha_mobile/comments/customer/views.py
+++ hoang_ha_mobile/comments/customer/views.py
@@ -5,10 +5,30 @@ from .. import serializers
 from rest_framework import generics, permissions, response, status
 from .. import models
 from django.shortcuts import get_object_or_404
+from rest_framework_simplejwt import authentication
 
 class CommentListOwner(generics.ListAPIView):
     serializer_class = serializers.CommentSerializer
-    
+    authentication_classes = [authentication.JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
     def get_queryset(self):
-        self.queryset = models.Comment.objects.filter(user = self.request.user.id)
+        self.queryset = models.Comment.objects.filter(phone = self.request.user.phone)
+        return super().get_queryset()
+    
+class CommentListProduct(generics.ListAPIView):
+    serializer_class = serializers.CommentSerializer
+    authentication_classes = [authentication.JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+    def get_queryset(self):
+        self.queryset = models.Comment.objects.filter(phone = self.request.user.phone, product = self.kwargs['product_id'])
+        # self.queryset = listObject.objects.filter()
+        return super().get_queryset()
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = serializers.CommentSerializer
+    authentication_classes = [authentication.JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+    lookup_url_kwarg = 'comment_id'
+    def get_queryset(self):
+        self.queryset = models.Comment.objects.filter(phone = self.request.user.phone)
         return super().get_queryset()
