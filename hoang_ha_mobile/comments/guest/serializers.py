2de1b670d8a392aa30cefa73da8609181eeb5eae
tantrung3t <tantrung.dmc@gmail.com>
2022-07-20 15:29:14 +0700

    complete comment for guest

---
+++ hoang_ha_mobile/comments/guest/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from ..models import Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    email = serializers.CharField(write_only=True)
+    phone = serializers.CharField(write_only=True)
+    class Meta:
+        model = Comment
+        fields = [
+            "id",
+            "name",
+            "email",
+            "phone",
+            "content",
+            "rating",
+            "created_at",
+            "product",
+        ]
---
+++ hoang_ha_mobile/comments/guest/views.py
@@ -0,0 +1,20 @@
+
+from itertools import product
+from rest_framework import generics, views
+from rest_framework.response import Response
+
+from products.models import Product
+
+from ..models import Comment
+from .serializers import CommentSerializer
+
+
+class ListCommentOfProductApiView(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        return Comment.objects.filter(product=self.request.query_params.get('product'))
+
+    def perform_create(self, serializer):
+        product = Product.objects.get(id=self.request.query_params.get('product'))
+        serializer.save(product=product)
