350e7f311e44248630442106c7238990e3f30aa1
Toan <crisriorock0@gmail.com>
2022-07-25 09:27:45 +0700

    update order detail

--- hoang_ha_mobile/orders/models.py
+++ hoang_ha_mobile/orders/models.py
@@ -6,7 +6,7 @@ User = get_user_model()
 
 class Order(models.Model):
     name = models.CharField(max_length=255)
-    phone = models.CharField(max_length=18,)
+    phone = models.CharField(max_length=10)
     email = models.EmailField()
     shipping = models.CharField(max_length=255)
     delivery_address = models.CharField(max_length=255)
@@ -31,12 +31,7 @@ class OrderDetail(models.Model):
     variant = models.ForeignKey(Variant, on_delete=models.CASCADE,related_name="order_variant_details")
     price = models.BigIntegerField()
     quantity = models.BigIntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, related_name ="order_detail_created" ,blank=True,null=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    updated_by = models.ForeignKey(User, on_delete=models.SET_NULL,related_name ="order_detail_updated", blank=True, null=True)
-    deleted_at = models.DateTimeField(blank=True,null=True)
-    deleted_by = models.ForeignKey(User, on_delete=models.SET_NULL,related_name ="order_detail_deleted", blank=True, null=True)
+    
 
 
     class Meta:
