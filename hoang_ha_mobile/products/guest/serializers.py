b23711e4563c8e69409e2c8a53884e0eda6920bb
tantrung3t <tantrung.dmc@gmail.com>
2022-07-27 10:24:56 +0700

    complete caterogy and product for guest

--- hoang_ha_mobile/products/guest/serializers.py
+++ hoang_ha_mobile/products/guest/serializers.py
@@ -3,17 +3,27 @@ from rest_framework import serializers
 
 from ..models import Product
 from categories.guest.serializers import ReadCategorySerializer
+from variants.models import Variant
+
+
+class VariantReadInProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Variant
+        fields = [
+            'id',
+            'image',
+            'color',
+            'version',
+            'price',
+            'sale',
+        ]
 
 class ReadProductSerializer(serializers.ModelSerializer):
-    category = ReadCategorySerializer()
     class Meta:
         model = Product
         fields = [
             "id",
             "name",
-            "description",
-            "insurance",
-            "category"
         ]
 
         read_only_fields = [
@@ -22,4 +32,30 @@ class ReadProductSerializer(serializers.ModelSerializer):
             "description",
             "insurance",
             "category"
+        ]
+
+class ShortProductSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "name",
+            "category"
+        ]
+class ReadDetailProductSerializer(serializers.ModelSerializer):
+    product = ShortProductSerializer()
+
+    # def get_product(self, obj):
+    #     print(obj.category)
+    #     return obj.id
+    class Meta:
+        model = Variant
+        fields = [
+            "id",
+            "product",
+            "version",
+            "price",
+            "sale",
+            "image"
         ]
--- hoang_ha_mobile/products/guest/views.py
+++ hoang_ha_mobile/products/guest/views.py
@@ -1,11 +1,23 @@
+from ast import Delete
+from multiprocessing import set_forkserver_preload
+from unicodedata import category
 from rest_framework import generics
-from .serializers import ReadProductSerializer
+from .serializers import ReadProductSerializer, ReadDetailProductSerializer
 from ..models import Product
+from variants.models import Variant
+
+
 class ListProductyApiView(generics.ListAPIView):
     serializer_class = ReadProductSerializer
-    queryset = Product.objects.filter(status=True).select_related('category')
+    pagination_class = None
 
-class DetailProductApiView(generics.RetrieveAPIView):
-    serializer_class = ReadProductSerializer
-    queryset = Product.objects.filter(status=True)
+    def get_queryset(self):
+        return Product.objects.filter(status=True, deleted_by=None, category=self.request.query_params.get('category')).select_related('category')
+
+
+class DetailProductApiView(generics.ListAPIView):
+    serializer_class = ReadDetailProductSerializer
     lookup_url_kwarg = 'product_id'
+
+    def get_queryset(self):
+        return Variant.objects.filter(deleted_by = None, status=True, product = self.kwargs.get('product_id'))
